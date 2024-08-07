d449682a9184ce6d774cf40e8c4c0943108d467f
snthshmnckm <snthshmnckm@gmail.com>
2024-08-07 22:50:14 +0530

    added some grainded beans

---
+++ sample/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hi there");
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+        System.out.printf("entered data is %.2f",a);
+    }
+}

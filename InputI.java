1c2046d4accd8defd27944a3a686347079f52b60
snthshmnckm <snthshmnckm@gmail.com>
2024-08-28 22:11:00 +0530

    Comman line args inputs

---
+++ InputI.java
@@ -0,0 +1,9 @@
+class InputI{
+    public static void main(String[] args){
+        if(args.length>0){
+            System.out.println(Integer.parseInt(args[0]));
+            System.out.println(Double.parseDouble(args[1]));
+        }
+       
+    }
+}
